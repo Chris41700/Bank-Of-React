c2432ff6856d1bac21ea002cb1d81f292fdd7e34
Chris41700 <ChrisHui417@gmail.com>
2022-01-12 15:51:07 -0500

    Home and AccountBalance components added

---
+++ src/components/AccountBalance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom';
+
+function AccountBalance() {
+    <>
+        return (
+            <div>
+                Balance: (this.props.accountBalance)
+            </div>
+        )
+    </>
+}
+
+export default AccountBalance;
